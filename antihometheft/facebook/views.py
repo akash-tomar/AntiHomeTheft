b03d7c5274894c1dd818db990ca3e4cabcd6979a
Akash Tomar <akash.tomar217@gmail.com>
2017-04-07 15:19:32 +0530

    post request for image handled

--- antihometheft/facebook/views.py
+++ antihometheft/facebook/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def getImage(request):
+	if request.method == 'POST':
+		id_of_user = request.POST.get('id')
+		image = request.POST.get('image')
+
+		#send this data to messeneger
